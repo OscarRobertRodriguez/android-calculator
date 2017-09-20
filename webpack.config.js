6849a91f34562cea78b521573049571925a060a9
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2017-09-20 14:09:13 -0500

    made changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+
+const extract = ExtractTextPlugin.extract({ use: 'css-loader' }); 
+
 const config = {
 	entry: './src/index.js',
 	output: {
